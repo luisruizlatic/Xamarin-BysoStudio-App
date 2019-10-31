b5cc2ad4404dc48a80b46741d28949ae84ebabaf
Luis Ruiz <luisruiz.latic@gmail.com>
2019-10-31 15:42:18 -0500

    Feat: Model Design based on MVVM Pattern

---
+++ BysoStudio/BysoStudio/Model/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BysoStudio.Model
+{
+    class Role
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Privileges { get; set; }
+
+    }
+}
---
+++ BysoStudio/BysoStudio/Model/WorkoutSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BysoStudio.Model
+{
+    class WorkoutSession
+    {
+        public int Id { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public int MaxCapacity { get; set; }
+        public int MinCapacity { get; set; }
+        public int PersonId { get; set; }
+        public int WorkoutId { get; set; }
+
+    }
+}
